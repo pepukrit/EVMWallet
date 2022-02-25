53bae3068052c1fdbb29e22d6fa7f8d80704473d
pepukrit <pepukrit@hotmail.com>
2022-02-25 21:26:35 +0700

    Initial Commit

---
+++ Shared/NFTWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFTWalletApp.swift
+//  Shared
+//
+//  Created by Ukrit Wattanakulchart on 25/2/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct NFTWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
