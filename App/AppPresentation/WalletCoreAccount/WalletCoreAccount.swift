f361317806232e16f66763f707864729022ae62d
pepukrit <pepukrit@hotmail.com>
2022-03-28 20:06:14 +0700

    Refactor WalletCoreAccountView

--- App/AppPresentation/WalletCoreAccount/WalletCoreAccount.swift
+++ App/AppPresentation/WalletCoreAccount/WalletCoreAccount.swift
@@ -23,139 +23,175 @@ struct WalletCoreAccount: View {
     
     var body: some View {
         NavigationView {
-            ZStack {
-                Color.primaryBgColor.ignoresSafeArea()
-                
-                VStack(spacing: 32) {
-                    Text("Wallet Core")
-                    
-                    VStack(spacing: 16) {
-                        Button(action: {
-                            walletCoreManager.createWallet(passphrase: passphrase) {
-                                print("Address: \($0)")
-                            }
-                        }) {
-                            Text("Create Wallet")
-                                .font(weight: .bold)
-                        }
-                        .padding()
-                        .background(Color.buttonBgColor)
-                        .foregroundColor(.white)
-                        .cornerRadius(16)
-                        
-                        Button(action: {
-                            accountBeingCreated = true
-                        }) {
-                            Text("Create Wallet With Mnemonic")
-                                .font(weight: .bold)
-                        }
-                        .padding()
-                        .background(Color.buttonBgColor)
-                        .foregroundColor(.white)
-                        .cornerRadius(16)
-                    }
-                    
-                    if let wallet = walletCoreManager.wallet {
-                        VStack(spacing: 16) {
-                            Text("Ethereum Address:")
-                            Text(wallet.getAddressForCoin(coin: .ethereum))
-                        }
-                        
-                        if let recentSignTransaction = recentSignTransaction {
-                            VStack(alignment: .leading, spacing: 16) {
-                                Text("Sign Transaction data")
-                                Text(recentSignTransaction)
-                            }
-                        }
-                        
-                        Button(action: {
-                            shouldShowSendView = true
-                        }) {
-                            Text("Send")
-                                .font(weight: .bold)
-                        }
-                        .padding()
-                        .background(Color.buttonBgColor)
-                        .foregroundColor(.white)
-                        .cornerRadius(12)
-                    }
-                    
-                    NavigationLink(destination: WalletCoreSendView(), isActive: $shouldShowSendView) {
-                        EmptyView()
-                    }
-                    
-                    Text("This Page is an attempt to use Wallet Core framework to interact with EVM")
-                        .multilineTextAlignment(.center)
-                        .font(with: 14, weight: .thin)
+            makeMainView()
+        }
+    }
+}
+
+private extension WalletCoreAccount {
+    
+    var sendViewNavigationLink: some View {
+        NavigationLink(destination: WalletCoreSendView(), isActive: $shouldShowSendView) {
+            EmptyView()
+        }
+    }
+    
+    var infoText: some View {
+        Text("This Page is an attempt to use Wallet Core framework to interact with EVM")
+            .multilineTextAlignment(.center)
+            .font(with: 14, weight: .thin)
+    }
+    
+    @ViewBuilder
+    func makeWalletCoreAccountHeadlineView() -> some View {
+        Text("Wallet Core")
+        
+        VStack(spacing: 16) {
+            Button(action: {
+                walletCoreManager.createWallet(passphrase: passphrase) {
+                    print("Address: \($0)")
                 }
-                .padding()
-                .font(with: 16, weight: .regular)
-                .foregroundColor(.white)
-                
-                if accountBeingCreated {
-                    ZStack {
-                        Color.black.opacity(0.6)
-                            .ignoresSafeArea()
-                            .onTapGesture {
-                                accountBeingCreated = false
-                            }
-                        
-                        VStack {
-                            Text("Please input your password")
-                                .font(with: 18, weight: .bold)
-                            
-                            VStack {
-                                VStack(spacing: 24) {
-                                    VStack {
-                                        SecureField("Input your passphrase", text: $passphrase)
-                                            .padding()
-                                            .background(Color.primaryBgColor)
-                                            .roundedClip()
-                                        
-                                        SecureField("Confirm your passphrase", text: $confirmPassphrase)
-                                            .padding()
-                                            .background(Color.primaryBgColor)
-                                            .roundedClip()
-                                    }
-                                    .font(weight: .regular)
-                                    .preferredColorScheme(.dark)
-                                    
-                                    Button(action: {
-                                        if validatePassphrase(passphrase1: passphrase, passphrase2: confirmPassphrase) {
-                                            walletCoreManager.createWallet(
-                                                from: "broom switch check angry army volume sugar crane plastic asset fantasy three",
-                                                passphrase: passphrase
-                                            ) {
-                                                print("Address: \($0)")
-                                                accountBeingCreated = false
-                                                
-                                            }
-                                        } else {
-                                            shouldShowAlertDialog = true
-                                        }
-                                    }) {
-                                        Text("Confirm")
-                                            .font(weight: .bold)
-                                    }
-                                    .padding()
-                                    .background(Color.buttonBgColor)
-                                    .roundedClip()
-                                }
-                                .padding()
-                                .background(Color.secondaryBgColor)
-                                .roundedClip()
-                            }
-                            .padding()
-                        }
-                        .foregroundColor(.white)
-                        .alert(isPresented: $shouldShowAlertDialog) {
-                            makeAlertFrom(passphrase1: passphrase, passphrase2: confirmPassphrase)
-                        }
-                    }
+            }) {
+                Text("Create Wallet")
+                    .font(weight: .bold)
+            }
+            .padding()
+            .background(Color.buttonBgColor)
+            .foregroundColor(.white)
+            .cornerRadius(16)
+            
+            Button(action: {
+                accountBeingCreated = true
+            }) {
+                Text("Create Wallet With Mnemonic")
+                    .font(weight: .bold)
+            }
+            .padding()
+            .background(Color.buttonBgColor)
+            .foregroundColor(.white)
+            .cornerRadius(16)
+        }
+    }
+    
+    @ViewBuilder
+    func makeOutputWalletCoreAccountView() -> some View {
+        if let wallet = walletCoreManager.wallet {
+            VStack(spacing: 16) {
+                Text("Ethereum Address:")
+                Text(wallet.getAddressForCoin(coin: .ethereum))
+            }
+            
+            if let recentSignTransaction = recentSignTransaction {
+                VStack(alignment: .leading, spacing: 16) {
+                    Text("Sign Transaction data")
+                    Text(recentSignTransaction)
                 }
             }
-            .navigationBarHidden(true)
+            
+            Button(action: {
+                shouldShowSendView = true
+            }) {
+                Text("Send")
+                    .font(weight: .bold)
+            }
+            .padding()
+            .background(Color.buttonBgColor)
+            .foregroundColor(.white)
+            .cornerRadius(12)
         }
+    }
+    
+    @ViewBuilder
+    func makePasswordConfirmationView() -> some View {
+        if accountBeingCreated {
+            ZStack {
+                Color.black.opacity(0.6)
+                    .ignoresSafeArea()
+                    .onTapGesture {
+                        accountBeingCreated = false
+                    }
+                
+                VStack {
+                    Text("Please input your password")
+                        .font(with: 18, weight: .bold)
+                    
+                    VStack {
+                        VStack(spacing: 24) {
+                            VStack {
+                                SecureField("Input your passphrase", text: $passphrase)
+                                    .padding()
+                                    .background(Color.primaryBgColor)
+                                    .roundedClip()
+                                
+                                SecureField("Confirm your passphrase", text: $confirmPassphrase)
+                                    .padding()
+                                    .background(Color.primaryBgColor)
+                                    .roundedClip()
+                            }
+                            .font(weight: .regular)
+                            .preferredColorScheme(.dark)
+                            
+                            Button(action: {
+                                if validatePassphrase(passphrase1: passphrase, passphrase2: confirmPassphrase) {
+                                    walletCoreManager.createWallet(
+                                        from: "broom switch check angry army volume sugar crane plastic asset fantasy three",
+                                        passphrase: passphrase
+                                    ) {
+                                        print("Address: \($0)")
+                                        accountBeingCreated = false
+                                        
+                                    }
+                                } else {
+                                    shouldShowAlertDialog = true
+                                }
+                            }) {
+                                Text("Confirm")
+                                    .font(weight: .bold)
+                            }
+                            .padding()
+                            .background(Color.buttonBgColor)
+                            .roundedClip()
+                        }
+                        .padding()
+                        .background(Color.secondaryBgColor)
+                        .roundedClip()
+                    }
+                    .padding()
+                }
+                .foregroundColor(.white)
+                .alert(isPresented: $shouldShowAlertDialog) {
+                    makeAlertFrom(passphrase1: passphrase, passphrase2: confirmPassphrase)
+                }
+            }
+        }
+    }
+    
+    @ViewBuilder
+    func makeDetailView() -> some View {
+        VStack(spacing: 32) {
+            makeWalletCoreAccountHeadlineView()
+            
+            makeOutputWalletCoreAccountView()
+            
+            sendViewNavigationLink
+            
+            infoText
+        }
+        .padding()
+        .font(with: 16, weight: .regular)
+        .foregroundColor(.white)
+    }
+    
+    @ViewBuilder
+    func makeMainView() -> some View {
+        ZStack {
+            Color.primaryBgColor.ignoresSafeArea()
+            
+            makeDetailView()
+            
+            makePasswordConfirmationView()
+        }
+        .navigationBarHidden(true)
     }
 }
 
