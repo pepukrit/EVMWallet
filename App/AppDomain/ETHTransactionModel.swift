7cb9d805ca3b296dd9508d782b3b97946a51eff8
pepukrit <pepukrit@hotmail.com>
2022-03-30 09:30:40 +0700

    Add ETHTransactionModel used to prepare necessary components before sending

---
+++ App/AppDomain/ETHTransactionModel.swift
@@ -0,0 +1,13 @@
+//
+//  ETHTransactionModel.swift
+//  NFTWallet (iOS)
+//
+//  Created by Ukrit Wattanakulchart on 28/3/2565 BE.
+//
+
+import Foundation
+
+struct ETHTransactionModel {
+    let nonce: String
+    let gasPrice: String
+}
